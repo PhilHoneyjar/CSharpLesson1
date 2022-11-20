e69a79ae28e568c80fffeb8b8ab5d1f3b8ea98f9
Philipp Medoev <118466438+PhilippMedoed@users.noreply.github.com>
2022-11-20 23:12:13 +0300

    Поправил условие

--- Задача 4.cs
+++ Задача 4.cs
@@ -1,9 +1,9 @@
-﻿// Ввести число a и показать все четные числа от -a до a
+﻿// Ввести число a и показать все четные числа от 1 до a
 
-Console.WriteLine("Введите целое число: ");
+Console.WriteLine("Введите целое положительное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Четные числа от -{a} до {a}: ");
-    for (int i = -a; i <= a; i++)
+Console.WriteLine($"Четные числа от 1 до {a}: ");
+    for (int i = 1; i <= a; i++)
     {
         for (int j = i; j % 2 == 0; j++)
         System.Console.WriteLine(j);
